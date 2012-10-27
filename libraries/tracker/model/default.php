6c8e497f741ad403317f19c91e30b658502697c2
Nikolai Plath <der.el.kuku@gmail.com>
2012-10-27 00:42:53 -0500

    Add a default model

---
+++ libraries/tracker/model/default.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     JTracker
+ * @subpackage  Model
+ *
+ * @copyright   Copyright (C) 2012 Open Source Matters. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Base model class.
+ *
+ * @package     JTracker
+ * @subpackage  Model
+ * @since       1.0
+ */
+class JModelDefault extends JModelBase
+{
+}
