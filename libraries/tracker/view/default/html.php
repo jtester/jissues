f4b204c031814aac1ec5dff59dd31f58a31df90e
Nikolai Plath <der.el.kuku@gmail.com>
2012-10-27 00:38:46 -0500

    Fix a docblock

--- libraries/tracker/view/default/html.php
+++ libraries/tracker/view/default/html.php
@@ -1,11 +1,21 @@
 <?php
 /**
- * User: elkuku
- * Date: 25.10.12
- * Time: 14:13
+ * @package     JTracker
+ * @subpackage  View
+ *
+ * @copyright   Copyright (C) 2012 Open Source Matters. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Base HTML view class.
+ *
+ * @package     JTracker
+ * @subpackage  Model
+ * @since       1.0
+ */
 class JViewDefaultHtml extends JViewHtml
 {
-
 }
