30abab749736489ed3cc148f0b9e1702305da92b
Nikolai Plath <der.el.kuku@gmail.com>
2012-10-28 19:35:32 -0500

    Simplify JControllerFormSave

--- administrator/components/com_tracker/controller/saveproject.php
+++ administrator/components/com_tracker/controller/saveproject.php
@@ -16,63 +16,6 @@ defined('_JEXEC') or die;
  * @subpackage  com_tracker
  * @since       1.0
  */
-class ComAdminTrackerControllerSaveproject extends JControllerTrackerform
+class ComAdminTrackerControllerSaveproject extends JControllerFormSave
 {
-	/**
-	 * This will call the "save" method of the parent class.
-	 *
-	 * @param   string  $task  NOT USED. @todo legacy
-	 *
-	 * @since   1.0
-	 * @throws  Exception
-	 *
-	 * @return  mixed   The value returned by the called method, false in error case.
-	 */
-	public function execute($task = '')
-	{
-		return parent::save();
-	}
-
-	/**
-	 * Method to get a model object, loading it if required.
-	 *
-	 * @param   string  $name    The model name. Optional. @todo legacy
-	 * @param   string  $prefix  The class prefix. Optional. @todo legacy
-	 * @param   array   $config  Configuration array for model. Optional. @todo legacy
-	 *
-	 * @return  object  The model.
-	 *
-	 * @since   1.0
-	 */
-	public function getModel($name = '', $prefix = '', $config = array('ignore_request' => true))
-	{
-		return new ComAdminTrackerModelProject;
-	}
-
-	/**
-	 * Gets the URL arguments to append to a list redirect.
-	 *
-	 * @return  string  The arguments to append to the redirect URL.
-	 *
-	 * @since   1.0
-	 */
-	protected function getRedirectToListAppend()
-	{
-		return '';
-	}
-
-	/**
-	 * Gets the URL arguments to append to an item redirect.
-	 *
-	 * @param   integer  $recordId  The primary key id for the item.
-	 * @param   string   $urlVar    The name of the URL variable for the id.
-	 *
-	 * @return  string  The arguments to append to the redirect URL.
-	 *
-	 * @since   1.0
-	 */
-	protected function getRedirectToItemAppend($recordId = null, $urlVar = 'id')
-	{
-		return '';
-	}
 }
