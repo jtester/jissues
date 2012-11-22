3cfad74a95e12aea1879106eb9541f85e5a9de90
Nikolai Plath <der.el.kuku@gmail.com>
2012-11-21 22:56:52 -0500

    Add an admin bootstrap.php

---
+++ administrator/includes/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package    JTracker
+ *
+ * @copyright  Copyright (C) 2012 Open Source Matters. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+require_once __DIR__ . '/defines.php';
+require_once JPATH_CONFIGURATION.'/configuration.php';
+require_once __DIR__ . '/framework.php';
+require_once __DIR__ . '/toolbar.php';
