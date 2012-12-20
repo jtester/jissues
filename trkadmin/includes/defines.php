1b729c468daaed4885ae274c407bf98534ea4c9f
Nikolai Plath <der.el.kuku@gmail.com>
2012-12-20 14:39:58 -0500

    Define JPATH_INSTALLATION in admin to prevent a notice

--- trkadmin/includes/defines.php
+++ trkadmin/includes/defines.php
@@ -27,3 +27,6 @@ define('JPATH_PLUGINS', JPATH_ROOT . '/plugins');
 define('JPATH_THEMES', JPATH_BASE . '/templates');
 define('JPATH_CACHE', JPATH_ROOT . '/cache');
 define('JPATH_MANIFESTS', JPATH_ADMINISTRATOR . '/manifests');
+
+// @legacy
+define('JPATH_INSTALLATION', '@legacy');
