f736b64c3471065bfe4d56cfdf8617be1164d2bc
Nikolai Plath <der.el.kuku@gmail.com>
2013-01-04 12:16:33 -0500

    STOP importing the legacy libraries in admin by default

--- trkadmin/includes/framework.php
+++ trkadmin/includes/framework.php
@@ -11,11 +11,15 @@ defined('_JEXEC') or die;
 // Import the application libraries, load it before the rest of the Platform to allow class overrides
 require_once JPATH_LIBRARIES . '/tracker.php';
 
+// Import the Joomla Platform with legacy support.
+require_once JPATH_LIBRARIES . '/import.php';
+
 // System includes.
-require_once JPATH_LIBRARIES.'/import.legacy.php';
+// @todo require_once JPATH_LIBRARIES . '/import.legacy.php';
+require_once JPATH_LIBRARIES . '/cms/version/version.php';
 
 // Botstrap the CMS libraries.
-require_once JPATH_LIBRARIES.'/cms.php';
+require_once JPATH_LIBRARIES . '/cms.php';
 
 // Register The universal CMS extension autoloader.
 spl_autoload_register(array('JCmsLoader', 'load'));
